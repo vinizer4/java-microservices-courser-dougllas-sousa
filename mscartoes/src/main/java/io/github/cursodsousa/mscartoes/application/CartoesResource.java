6ff3bbe41bf69f1f23194c79f16f1ef80a43b975
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-14 02:22:32 -0300

    mscartoes: configurado no eureka

---
+++ mscartoes/src/main/java/io/github/cursodsousa/mscartoes/application/CartoesResource.java
@@ -0,0 +1,15 @@
+package io.github.cursodsousa.mscartoes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cartoes")
+public class CartoesResource {
+
+    @GetMapping
+    public String status() {
+        return "API Cartões funcionando!";
+    }
+}
