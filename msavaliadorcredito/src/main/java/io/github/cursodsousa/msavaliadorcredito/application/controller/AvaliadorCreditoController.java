e0950a9f7959dc946384b52062befc09fd788e28
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-15 14:26:51 -0300

    msavaliadorcredito: comunicacao sincrona entre microservicos usando feign client

--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/controller/AvaliadorCreditoController.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/controller/AvaliadorCreditoController.java
@@ -1,17 +1,32 @@
 package io.github.cursodsousa.msavaliadorcredito.application.controller;
 
+import io.github.cursodsousa.msavaliadorcredito.application.service.AvaliadorCreditoService;
+import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
+@RequiredArgsConstructor
 public class AvaliadorCreditoController {
+
+    private final AvaliadorCreditoService avaliadorCreditoService;
 
     @GetMapping
     public String status() {
         return "Serviço de avaliação de crédito está funcionando!";
     }
 
+
+    @GetMapping(value = "situacao-cliente", params = "cpf")
+    public ResponseEntity<SituacaoCliente> consultarSituacaoCliente(@RequestParam("cpf") String cpf) {
+        SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+
+        return ResponseEntity.ok(situacaoCliente);
+    }
 
 }
---
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/infra/clients/ClienteResourceClient.java
@@ -0,0 +1,17 @@
+package io.github.cursodsousa.msavaliadorcredito.infra.clients;
+
+import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "msclientes", path = "/clientes")
+public interface ClienteResourceClient {
+
+    @GetMapping
+    String status();
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<DadosCliente> dadosCliente(@RequestParam("cpf") String cpf);
+}
