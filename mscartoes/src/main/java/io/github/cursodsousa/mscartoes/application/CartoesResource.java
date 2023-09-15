ea8b2b4acce8feda981a3bb793dec32b06473380
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-15 00:21:27 -0300

    mscartoes: criado endpoints criar e obter cartoes

--- mscartoes/src/main/java/io/github/cursodsousa/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/io/github/cursodsousa/mscartoes/application/CartoesResource.java
@@ -1,15 +1,29 @@
 package io.github.cursodsousa.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.cursodsousa.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.cursodsousa.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cartoes")
+@RequiredArgsConstructor
 public class CartoesResource {
+
+    private final CartaoService cartaoService;
 
     @GetMapping
     public String status() {
         return "API Cartões funcionando!";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request) {
+        Cartao cartao = request.toModel();
+
+        cartaoService.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
