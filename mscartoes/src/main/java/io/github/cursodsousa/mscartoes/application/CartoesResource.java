e6b91dfc930fc6192208a00c7f5fce2f1ceb92b3
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-15 00:45:31 -0300

    mscartoes: criado endpoint obter cartoes por cliente

--- mscartoes/src/main/java/io/github/cursodsousa/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/io/github/cursodsousa/mscartoes/application/CartoesResource.java
@@ -1,13 +1,16 @@
 package io.github.cursodsousa.mscartoes.application;
 
 import io.github.cursodsousa.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.cursodsousa.mscartoes.application.representation.CartoesPorClienteResponse;
 import io.github.cursodsousa.mscartoes.domain.Cartao;
+import io.github.cursodsousa.mscartoes.domain.ClienteCartao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cartoes")
@@ -15,6 +18,7 @@ import java.util.List;
 public class CartoesResource {
 
     private final CartaoService cartaoService;
+    private final ClienteCartaoService clienteCartaoService;
 
     @GetMapping
     public String status() {
@@ -33,5 +37,15 @@ public class CartoesResource {
     public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda) {
         List<Cartao> cartoes = cartaoService.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok(cartoes);
+    }
+
+    @GetMapping("/getByClienteCpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(@RequestParam String cpf) {
+        List<ClienteCartao> listaCartoes = clienteCartaoService.listCartoesByCpf(cpf);
+        List<CartoesPorClienteResponse> resultList = listaCartoes.stream()
+                .map(CartoesPorClienteResponse::fromModel)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(resultList);
     }
 }
